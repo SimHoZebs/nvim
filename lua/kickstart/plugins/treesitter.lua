3e4037e2a3e7bda472f8842eefd00fe4f1165843
Sim Ho <simhozebs@gmail.com>
2024-03-13 13:13:56 -0400

    ignore yaml; its tree sitter is not working

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -8,6 +8,7 @@ return {
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
         ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ignore_install = { 'yaml' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
