ee6baba6d0251d0d402098181c592d23069bc41f
Sim Ho <simhozebs@gmail.com>
2024-03-13 13:13:38 -0400

    fix formatter not working

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,20 +1,22 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 50000,
+        async = true,
         lsp_fallback = true,
       },
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        typescriptreact = { { 'prettierd', 'prettier' } },
+        javascriptreact = { { 'prettierd', 'prettier' } },
+        json = { 'prettierd' },
+        astro = { 'prettierd' },
       },
     },
   },
