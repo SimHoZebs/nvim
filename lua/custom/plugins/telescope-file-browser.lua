4611e4e40592e309b9529de0f6ce41205f0b6fed
Sim Ho <simhozebs@gmail.com>
2024-04-04 14:11:38 -0400

    unify file_browser with telescope bindings and add preview toggle keybind

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -1,5 +1,18 @@
 return {
   'nvim-telescope/telescope-file-browser.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-  vim.api.nvim_set_keymap('n', '<space>fb', ':Telescope file_browser<CR>', { noremap = true }),
+  vim.api.nvim_set_keymap('n', '<space>sb', ':Telescope file_browser<CR>', { noremap = true, desc = '[S]earch file [B]rowser' }),
+  config = function()
+    require('telescope').setup {
+      extensions = {
+        file_browser = {
+          mappings = {
+            i = {
+              ['<C-p>'] = require('telescope.actions.layout').toggle_preview,
+            },
+          },
+        },
+      },
+    }
+  end,
 }
