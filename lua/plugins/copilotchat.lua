3e00c556c5af57c247dec87f878b2a8dbc014e27
SimHoZebs <simhozebs@gmail.com>
2024-10-08 09:15:07 -0400

    copilot chat fix

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -4,13 +4,49 @@ return {
     branch = "canary",
     event = "VeryLazy",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
     },
+    build = "make tiktoken",
     opts = {
       debug = true, -- Enable debugging
-      -- See Configuration section for rest
+      -- Adding key mappings for lazy.nvim
     },
-    -- See Commands section for default commands if you want to lazy load on them
-  },
+    keys = {
+      -- Show help actions with telescope
+      {
+        "<leader>ch",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+        end,
+        desc = "CopilotChat - Help actions",
+      },
+      -- Show prompts actions with telescope
+      {
+        "<leader>cp",
+        function()
+          local actions = require("CopilotChat.actions")
+          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+        end,
+        desc = "CopilotChat - Prompt actions",
+      },
+      -- Open CopilotChat window
+      {
+        "<leader>cl",
+        function()
+          require("CopilotChat").open()
+        end,
+        desc = "Open CopilotChat",
+      },
+      -- Reset CopilotChat window
+      {
+        "<leader>cr",
+        function()
+          require("CopilotChat").reset()
+        end,
+        desc = "Reset CopilotChat",
+      },
+    },
+  }
 }
