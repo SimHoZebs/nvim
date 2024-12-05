c7d1b1ca2723eadf88a216323c75ff0a4ebd977e
SimHoZebs <simhozebs@gmail.com>
2024-12-05 11:04:27 -0500

    copilot disable

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,5 +6,15 @@ return {
         replace_keycodes = false
       })
       vim.g.copilot_no_tab_map = true
+      -- disable copilot with leader c d by running command Copilot disable
+      vim.keymap.set('n','<leader>cd',':Copilot disable<CR>', {
+        noremap = true,
+        silent = true
+      }) 
+      -- enable
+      vim.keymap.set('n','<leader>ce',':Copilot enable<CR>', {
+        noremap = true,
+        silent = true
+      })
   end,
 }
